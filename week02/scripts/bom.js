98e815fb1d90ac2bbb851f90576572e2c1c9d9d2
larryg21 <lgomezvillamil@byupathway.edu>
2025-09-14 19:01:30 -0400

    updated

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,32 +1,28 @@
-// Variables
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('#list');
+const list = document.querySelector('ul'); 
 
-//  "Add Chapter" button
 button.addEventListener('click', () => {
-    
+
     if (input.value.trim() !== '') {
-        // DOM elements
+
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
 
-        // list item and delete button
         li.textContent = input.value;
         deleteButton.textContent = '❌';
 
         li.append(deleteButton);
-       
+
         list.append(li);
 
         deleteButton.addEventListener('click', () => {
             list.removeChild(li);
-            input.focus(); // 
+            input.focus(); 
         });
 
-        // Clear the input 
         input.value = '';
     }
-
+    
     input.focus();
 });
