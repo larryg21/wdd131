00e297615b60d9b865f1e90b0e8f53abd6625b64
larryg21 <lgomezvillamil@byupathway.edu>
2025-09-14 18:51:57 -0400

    Book of Mormon Top 10

---
+++ week02/scripts/bom.js
@@ -0,0 +1,18 @@
+// Variables
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+// DOM elements
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+
+
+li.append(deleteButton);
+
+
+list.append(li);
